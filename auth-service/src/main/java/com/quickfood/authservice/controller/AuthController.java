9e37be26f00e698a19ca440c66a5ef94099b2e52
NguyenLT <letrungnguyen1004@gmail.com>
2026-08-04 10:51:06 +0700

    init: init dev branch

--- auth-service/src/main/java/com/quickfood/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/quickfood/authservice/controller/AuthController.java
@@ -8,6 +8,8 @@ import com.quickfood.authservice.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -28,4 +30,13 @@ public class AuthController {
         AuthResponse authResponse = authService.login(request);
         return ResponseEntity.ok(ApiResponse.success("Login successful!", authResponse));
     }
+
+    @GetMapping("/me")
+    public ResponseEntity<ApiResponse<String>> getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUsername = authentication.getName();
+        
+        return ResponseEntity.ok(ApiResponse.success("Access granted!", "Current user is: " + currentUsername));
+    }
+
 }
